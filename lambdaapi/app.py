0a0be894648d51e9f8ecdd19e36e78768164421a
Ravishankar Sivasubramaniam <ravishankar.sivasubramaniam@nielsen.com>
2017-04-02 14:21:50 -0500

    Pylint added

--- lambdaapi/app.py
+++ lambdaapi/app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import boto3
+import datetime
+import json
+import logging
+import os
 from chalice import Chalice
-import boto3,json,os,logging,datetime
 
 app = Chalice(app_name='lambdaapi')
 app.log.setLevel(logging.DEBUG)
@@ -10,15 +14,22 @@ app.log.setLevel(logging.DEBUG)
 S3 = boto3.client('s3', region_name=os.getenv('REGION_NAME'))
 BUCKET_NAME = os.getenv('BUCKET_NAME')
 
+
+
+"""
+echo End Point take name and returns name as json
+"""
 @app.route('/echo/{name}')
 def echo(name):
-    app.log.debug("Calling /echo/%s",name)
+    app.log.debug("Calling /echo/%s", name)
     request = app.current_request
 
-    S3.put_object(Bucket=BUCKET_NAME, Key=name+'_'+str(datetime.datetime.now()),
+    S3.put_object(Bucket=BUCKET_NAME, Key=name + '_' + str(datetime.datetime.now()),
                   Body=json.dumps(request.to_dict()))
     return {'hello': name}
 
-
+"""
+    Function to Count
+"""
 def count():
     return 1
